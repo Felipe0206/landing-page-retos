62a64c382166cd32604e75ac1d2fd865a379a4ea
unknown <conocimiento.02@gmail.com>
2021-11-02 00:27:35 -0500

    landing-page-retos

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import style from './Header.module.css'
+
+function Header() {
+  return (
+    <div className={style.heroImage}>
+      
+      <h2>25 Retos </h2>
+      
+      
+
+    </div>
+  )
+
+}
+
+export default Header
